5b8c9d31d94578cdcedfa98674a310c3f1689a23
safdar968 <muhammadsafdar5747@gmail.com>
2020-02-22 14:05:06 +0500

    updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # RPi_GSM_Robot
-RPi based robot with GSM and GUI Control
+## RPi based robot with GSM and GUI Control
+
+Important resources
+https://robu.in/interfacing-of-gps-module-with-arduino-and-raspberry-pi/
--- main.py
+++ main.py
@@ -1,70 +1,143 @@
 import tkinter
 from tkinter import filedialog
+import math, time
+import serial
+import pynmea2 
+from i2clibraries import i2c_hmc5883l
+
+
+class ROBOT():
+    def __init__(self):
+        self.lat = 0.0
+        self.lng = 0.0
+        self.headingDeg = 0
+        self.headingMin = 0
+
+        port="/dev/serial0"
+        self.ser=serial.Serial(port, baudrate=9600, timeout=0.5) 
+        dataout =pynmea2.NMEAStreamReader()
+        self.hmc5883l = i2c_hmc5883l.i2c_hmc5883l(1)
+        self.hmc5883l.setContinuousMode()
+        self.hmc5883l.setDeclination(2,25)
+
+    def getGPSData(self):
+        newdata=self.ser.readline()
+        #print(newdata)
+        if newdata[0:6] == "$GPRMC":  
+            newmsg=pynmea2.parse(newdata)  
+            self.lat=newmsg.latitude 
+            self.lng=newmsg.longitude 
+            gps = "Latitude=" + str(self.lat) + "and Longitude=" +str(self.lng) 
+            print(gps)
+
+    def getGPSHeading(self):
+        self.headingDeg, self.headingMin = self.hmc5883l.getHeading()
+        print(str(self.headingDeg) + " deg, " + str(self.headingMin) + " minutes")
+
+    def distanceFromCoordinates(self, lat1, long1, lat2, long2):
+        radius=6371 # radius of earth
+        # converion to radians
+        lat1=lat1*math.pi/180
+        lat2=lat2*math.pi/180
+        lon1=long1*math.pi/180
+        lon2=long2*math.pi/180
+
+        deltaLat=lat2-lat1
+        deltaLon=lon2-lon1
+
+        a=math.sin((deltaLat)/2)^2 + math.cos(lat1)*math.cos(lat2) * math.sin(deltaLon/2)^2
+        c=2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+        return radius*c    #Haversine distance
+
+    def angleFromCoordinate(self, lat1, long1, lat2, long2):
+        dLon = (long2 - long1)
+        y = math.sin(dLon) * math.cos(lat2)
+        x = math.cos(lat1) * math.sin(lat2) - math.sin(lat1) * math.cos(lat2) * math.cos(dLon)
+        brng = math.atan2(y, x)
+        brng = math.degrees(brng)
+        brng = (brng + 360) % 360
+        brng = 360 - brng # count degrees clockwise - remove to make counter-clockwise
+        return brng
 
 class App:
     def __init__(self, window, window_title):
         self.window = window
-        self.initTracking = None
         self.window.title(window_title)
-        self.video_source = ''
-        self.input_options = [
-            ('CSRT',1),
-            ('MIL', 2)]
+        self.input_type = tkinter.IntVar()
+        self.input_type.set(0)
 
-        self.tracker_val = tkinter.IntVar()
-        self.tracker_val.set(0)
-
-        # Create a canvas that can fit the above video source size
-        self.canvas = tkinter.Canvas(window, relief="sunken", width = 640, height = 480)
-        self.canvas.bind("<Button-1>", self.set_tracker_type)
-        rtsplbl = tkinter.Label(window, text="RTSP Link:")
-        resultlbl = tkinter.Label(window, text="Result:")
-        threshlbl = tkinter.Label(window, text="Detector Threshold:")
-        self.thresh = tkinter.Entry(window)
-        self.link = tkinter.Entry(window)
-        self.result = tkinter.Text(window, state='disabled', width=10, height=1)
-        self.btn_set_tracker=tkinter.Button(window, text="Set Tracker", width=10, command=self.set_tracker_type)
-        self.btn_load_file = tkinter.Button(window, text="Load Video", width=10, command=self.load_file)
-        self.btn_track = tkinter.Button(window, text="Track", width=10, command=self.track)
-        # Button that lets the user take a select the object to track
-        for val, tracker_type in enumerate(self.input_options):
-
-            # Radio Button that lets the user select tracker type
-            radio_tracker_type = tkinter.Radiobutton(window, 
-                                        text=tracker_type[0],
+        # Input Side
+        latlbl_i = tkinter.Label(window, text="Latitude:")
+        longlbl_i = tkinter.Label(window, text="Longitude:")
+        setlbl = tkinter.Label(window, text="Set Coordinates:")
+        getlbl = tkinter.Label(window, text="General Status:")
+        cord_listlbl = tkinter.Label(window, text="Coordinates List:")
+        self.cord_list = tkinter.Text(window, state='disabled', width=10, height=5)
+        self.lat_input = tkinter.Entry(window)
+        self.long_input = tkinter.Entry(window)
+        self.btn_open_map=tkinter.Button(window, text="Open Map", width=10, command=self.open_map)
+        self.btn_add_manual = tkinter.Button(window, text="Add", width=10, command=self.add_manual)
+        self.btn_start_robot=tkinter.Button(window, text="START", width=10, command=self.open_map)
+        self.btn_stop_robot = tkinter.Button(window, text="STOP", width=10, command=self.add_manual)
+        radio_open_map = tkinter.Radiobutton(window, 
+                                        text='Select on Map',
                                         padx = 20, 
-                                        variable=self.tracker_val, 
-                                        value=val
+                                        variable=self.input_type, 
+                                        value=0
                                         )
-            radio_tracker_type.grid(column=5, row=val, sticky='NW')
 
+        radio_manual = tkinter.Radiobutton(window, 
+                                        text='Manual Selection',
+                                        padx = 20, 
+                                        variable=self.input_type, 
+                                        value=1
+                                        )
 
-        self.canvas.grid(column=0, row=0, columnspan=5, rowspan=20)
-        self.link.grid(column=1, row=21, sticky='WE')
-        rtsplbl.grid(column=0, row=21)
-        self.result.grid(column=5, row=17, sticky='NWE')
-        resultlbl.grid(column=5, row=16, sticky='SW')
-        self.thresh.grid(column=5, row=12, sticky='NWE')
-        threshlbl.grid(column=5, row=11, sticky='SW')
-        self.btn_set_tracker.grid(column=5, row=19, sticky='WSE')
-        self.btn_load_file.grid(column=5, row=18, sticky='WSE')
+        # Output Side
+        current_loclbl_o = tkinter.Label(window, text="Current Location:")
+        latlbl_o = tkinter.Label(window, text="Latitude:")
+        longlbl_o = tkinter.Label(window, text="Longitude:")
+        speedlbl_o = tkinter.Label(window, text="Speed:")
+        self.lat_output = tkinter.Text(window, state='disabled', width=10, height=1)
+        self.long_output = tkinter.Text(window, state='disabled', width=10, height=1)
+        self.speed_output = tkinter.Text(window, state='disabled', width=10, height=1)
+
+        #input side grid
         
+        setlbl.grid(column=0, row=0, sticky='NSWE')
+        radio_open_map.grid(column=1, row=1, sticky='NSW')
+        radio_manual.grid(column=1, row=2, sticky='NSW')
+        self.btn_open_map.grid(column=2, row=1, sticky='NS')
+        self.btn_add_manual.grid(column=3, row=4, sticky='NS')
+        latlbl_i.grid(column=2, row=2)
+        longlbl_i.grid(column=2, row=3, sticky='NSWE')
+        self.long_input.grid(column=3, row=2, sticky='NSWE')
+        self.lat_input.grid(column=3, row=3, sticky='NSWE')
+        self.cord_list.grid(column=1, row=9, sticky='NSWE')
+        cord_listlbl.grid(column=0, row=8, sticky='NSWE')
 
-            
+        #output side grid
+        getlbl.grid(column=4, row=0, sticky='NSW')
+        self.lat_output.grid(column=6, row=3, sticky='NSWE')
+        self.long_output.grid(column=7, row=3, sticky='NSWE')
+        self.speed_output.grid(column=6, row=4, sticky='NSWE')
+        current_loclbl_o.grid(column=5, row=3, sticky='NSW')
+        latlbl_o.grid(column=6, row=2, sticky='NSWE')
+        longlbl_o.grid(column=7, row=2, sticky='NSWE')
+        speedlbl_o.grid(column=5, row=4, sticky='NSW')
+        self.btn_start_robot.grid(column=2, row=10, sticky='NS')
+        self.btn_stop_robot.grid(column=4, row=10, sticky='NS')
+        
         # After it is called once, the update method will be automatically called every delay milliseconds
         self.delay = 1
         self.update()
-
         self.window.mainloop()
 
 
-    def set_tracker_type(self):
+    def open_map(self):
         pass
 
-    def load_file(self):
-        pass
-
-    def track(self):
+    def add_manual(self):
         pass
 
     def update(self):
